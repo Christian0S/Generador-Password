b2704ce07951a12f6d239c4dce7117f8e313fb1f
Christian0S <christian.s.mojica@gmail.com>
2024-09-17 21:14:52 -0500

    secon commit

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
+let  cantidad = document.getElementById('cantidad');
+let boton = document.getElementById('generar');
+let contraseNa = document.getElementById('contrasena')
+
+const cadenaDeCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 
+function generar (){
 
-
-
-
-
-
-
+    let numeroDigitado = parseInt (cantidad.value);
+    console.log(numeroDigitado);
+    if (numeroDigitado < 8){
+        alert("La cantidad de caracteres tiene que ser mayor que 8")
+    }
+    let password = "";
+    for (let i = 0;  i < numeroDigitado ; i++){
+        let caraterAletorio = cadenaDeCaracteres[Math.floor(Math.random() * cadenaDeCaracteres.length)];
+        console.log(caraterAletorio);
+        password+=caraterAletorio;
+    }
+    contraseNa.value = password;
+}
